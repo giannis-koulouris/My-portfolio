5cdeb7909a927a1420b27a0431f81d02e371b986
giannis-koulouris <gianiskoulouris@outlook.com>
2021-09-04 17:43:29 +0300

    changed folder structure, added light mode by default, better colors

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
+// THEME JS
 const THEME_TOGGLE_BTN = document.getElementById('theme-toggle');
-
-
-
-
 THEME_TOGGLE_BTN.addEventListener('change', (e)=> {
     if(e.target.checked) {
-        document.body.classList.add('light');
-        localStorage.setItem('theme', 'light');
-    }else{
-        document.body.classList.remove('light');
+        document.body.classList.add('dark');
         localStorage.setItem('theme', 'dark');
+    }else{
+        document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
     }
 });
 
@@ -17,13 +14,33 @@ THEME_TOGGLE_BTN.addEventListener('change', (e)=> {
 let setActiveTheme = ()=> {
     const THEME = localStorage.getItem('theme');
 
-    if(THEME === 'light') {
-        document.body.classList.add('light');
+    if(THEME === 'dark') {
+        document.body.classList.add('dark');
         THEME_TOGGLE_BTN.checked = true;
     }else{
-        document.body.classList.remove('light');
+        document.body.classList.remove('dark');
         THEME_TOGGLE_BTN.checked = false;
     }
 }
 
 setActiveTheme();
+
+
+
+// SMOOTH SCROLLING JS
+const links = document.querySelectorAll(".site-navigation a");
+ 
+for (let link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
