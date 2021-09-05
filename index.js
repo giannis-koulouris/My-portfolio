e1b0878cff9e329069f7de731582c2a35f9126d2
giannis-koulouris <gianiskoulouris@outlook.com>
2021-09-05 19:06:01 +0300

    Website finished

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ setActiveTheme();
 
 
 // SMOOTH SCROLLING JS
-const links = document.querySelectorAll(".site-navigation a");
+const links = document.querySelectorAll(".site-navigation a, .mob-navigation a");
  
 for (let link of links) {
     link.addEventListener("click", clickHandler);
@@ -40,7 +40,69 @@ function clickHandler(e) {
     const offsetTop = document.querySelector(href).offsetTop;
 
     scroll({
-        top: offsetTop,
+        top: offsetTop - 40,
         behavior: "smooth"
     });
+
+    closeMobMenu();
 }
+
+
+// SLIDERS
+document.addEventListener('DOMContentLoaded' , ()=>{
+    let projectsSlider = new Swiper(".projects-slider", {
+        grabCursor: true,
+        slidesPerView: 1.2,
+        spaceBetween: 15,
+        breakpoints: {
+            480: {
+                slidesPerView: 2.2,
+            },
+            767: {
+                slidesPerView: 3.2,
+            },
+            1024: {
+                slidesPerView: 4.2,
+                spaceBetween: 20,
+            },
+        }
+      });
+});
+
+
+// MOBILE MENU
+const MOB_MENU_TOGGLE_BTN = document.querySelector('.mob-menu-toggle');
+const MOB_NAVIGATION = document.querySelector('.mob-navigation');
+
+let isMobMenuOpen = false; // store mob menu state
+
+// Open mob menu on burger button click
+MOB_MENU_TOGGLE_BTN.addEventListener('click' , e=> {
+    if(!isMobMenuOpen) {
+        MOB_MENU_TOGGLE_BTN.classList.add('open');
+        MOB_NAVIGATION.classList.add('open');
+        isMobMenuOpen = true;
+    }else{
+        closeMobMenu();
+    }
+});
+
+// Function to close mob menu
+function closeMobMenu() {
+    if(isMobMenuOpen) {
+        MOB_MENU_TOGGLE_BTN.classList.remove('open');
+        MOB_NAVIGATION.classList.remove('open');
+    }
+    isMobMenuOpen = false;
+}
+
+
+// Listen for all clicks on the document
+document.addEventListener('click', function(e) {
+
+	// close mob navigation when clicking outside
+	if (!e.target.closest('.mob-menu-toggle') && !e.target.closest('.mob-navigation')) {
+        closeMobMenu();
+	}
+
+}, false);
